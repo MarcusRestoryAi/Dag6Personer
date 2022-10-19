445a635fd9ca457df13fd566274aef9945a4546d
Marcus <marcus.henriksson@restory.ai>
2022-10-19 11:46:34 +0200

    Add project files.

---
+++ Dag6Personer/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag6Personer
+{
+    internal class Person
+    {
+        //Attribut
+        public string fName;
+        public string lName;
+        public int age;
+
+        //Konstruktor
+        public Person(string fName, string lName, int age)
+        {
+            this.fName = fName;
+            this.lName = lName;
+            this.age = age;
+        }
+
+        public string Greetings()
+        {
+            string output = "Mitt namn är "+fName+" "+lName+" och jag är "+age+" år gammal!";
+            return output;
+            
+            // return "Mitt namn är "+fName+" "+lName+" och jag är "+age+" är gammal!";
+        }
+
+        public string HelloToYou(Person otherPerson)
+        {
+            string oName = otherPerson.fName;
+            string output = "Hejsan "+oName+". Mitt namn är "+fName;
+            return output;
+        }
+    }
+}
---
+++ Dag6Personer/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Dag6Personer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initiera en lista för Person Objekt
+            List<Person> listPerson = new List<Person>();
+
+            //Skapa ett obejkt av klassen Person
+            //Spara obejktet i listan
+            listPerson.Add(new Person("Marcus", "Henriksson", 34));
+            listPerson.Add(new Person("Cihan", "Yasar", 49));
+            listPerson.Add(new Person("Niklas", "Cullberg", 32));
+            listPerson.Add(new Person("Bert", "Karlsson", 72));
+
+            //Loop för att gå igenom Personer som skall presentera
+            //sig själva
+            foreach(Person person in listPerson)
+            {
+                Console.WriteLine(person.Greetings()); 
+            }
+
+            //Skapa en ForEach loop för Person
+            foreach(Person p1 in listPerson)
+            {
+                //Skapa en inner loop för Person 2
+                foreach(Person p2 in listPerson)
+                {
+                    //If sats för att kontrollera att p1 och p2 är olika personer
+                    if (p1 != p2)
+                    {
+                        //p1 hälsar till p2
+                        Console.WriteLine(p1.HelloToYou(p2));
+                    }
+                }
+            }
+        }
+    }
+}
